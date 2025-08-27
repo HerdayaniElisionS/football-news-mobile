46bff4baacd0dad8a85dc1efc4dd8b7dec9a671b
HerdayaniElisionS <elisionstherdayani@gmail.com>
2025-08-27 16:20:40 +0700

    Fix ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for PWS

--- football_news/settings.py
+++ football_news/settings.py
@@ -39,6 +39,10 @@ ALLOWED_HOSTS = [
     "herdayani-elision-footballnews.pbp.cs.ui.ac.id",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://herdayani-elision-footballnews.pbp.cs.ui.ac.id",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
